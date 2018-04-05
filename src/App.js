6fedb8569653c9928676e136c95d73ee9c098933
Jake Spurlock <whyisjake@gmail.com>
2018-04-04 21:42:59 -0700

    Update the text

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to the Social Restraint Club</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Erika Fang, Jake Spurlock, and Roni Willet
         </p>
       </div>
     );
